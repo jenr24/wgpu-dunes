436942f4f466331b096c33cdd249079ba4ad04a9
Jen Reiss <jenc.reiss24@gmail.com>
2022-05-21 05:45:27 -0500

    nixfmt + added shellHook

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "dune heightmap generator for wasm using wgpu";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -15,50 +15,60 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, flake-compat, rust-overlay }:
-  flake-utils.lib.eachDefaultSystem(system:
-    let
-      overlays = [ rust-overlay.overlay ];
-      pkgs = import nixpkgs {  inherit system overlays; };
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ rust-overlay.overlay ];
+        pkgs = import nixpkgs { inherit system overlays; };
 
-      rust = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
 
-      rustPlatform = pkgs.makeRustPlatform {
-        cargo = rust;
-        rustc = rust;
-      };
-
-      dependencies = with pkgs; 
-        [ rust rust-analyzer rustfmt pkg-config wasm-bindgen-cli ];
-
-    in {
-
-      defaultPackage = rustPlatform.buildRustPackage rec {
-        pname = "wgpu_dunes";
-        version = "0.0.1";
-
-        nativeBuildInputs = dependencies;
-
-        src = ./.;
-
-        buildPhase = ''
-          cargo build --release --target=wasm32-unknown-unknown
-        '';
-        
-        installPhase = ''
-          echo 'Creating out dir...'
-          mkdir -p $out/lib;
-
-          echo 'Installing wasm module to $out/lib/'
-          cp target/wasm32-unknown-unknown/release/${pname}.wasm $out/lib/;
-        '';
-
-        cargoLock = {
-          lockFile = ./Cargo.lock;
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rust;
+          rustc = rust;
         };
 
-        verifyCargoDeps = true;
-      };
-      devShell = pkgs.mkShell { packages = dependencies; };
-    }
-  );
+        dependencies = with pkgs; [
+          rust
+          rust-analyzer
+          rustfmt
+          nixfmt
+          pkg-config
+          wasm-bindgen-cli
+          wasm-pack
+          openssl
+        ];
+
+      in {
+
+        defaultPackage = rustPlatform.buildRustPackage rec {
+          pname = "wgpu_dunes";
+          version = "0.0.1";
+
+          nativeBuildInputs = dependencies;
+
+          src = ./.;
+
+          buildPhase = ''
+            cargo build --release --target=wasm32-unknown-unknown
+          '';
+
+          installPhase = ''
+            echo 'Creating out dir...'
+            mkdir -p $out/lib;
+
+            echo 'Installing wasm module to $out/lib/'
+            cp target/wasm32-unknown-unknown/release/${pname}.wasm $out/lib/;
+          '';
+
+          cargoLock = { lockFile = ./Cargo.lock; };
+
+          verifyCargoDeps = true;
+        };
+        devShell = pkgs.mkShell { 
+          packages = dependencies; 
+          shellHook = ''
+            export PATH=~/.cache/.wasm-pack/.wasm-bindgen-cargo-install-0.2.80/bin:$PATH
+          '';
+        };
+      });
 }
